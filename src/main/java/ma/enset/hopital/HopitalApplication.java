f8c13b70aa44fc33747e40e57a7d93a206514e6c
oussamabartil <oussamabartil.04@gmail.com>
2025-03-17 10:11:57 +0000

    Test de l'ajout des patients avec différentes approches

--- src/main/java/ma/enset/hopital/HopitalApplication.java
+++ src/main/java/ma/enset/hopital/HopitalApplication.java
@@ -1,13 +1,42 @@
 package ma.enset.hopital;
 
+import ma.enset.hopital.entities.Patient;
+import ma.enset.hopital.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+
 @SpringBootApplication
-public class HopitalApplication {
+public class HopitalApplication implements CommandLineRunner {
+    @Autowired
+    private PatientRepository patientRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(HopitalApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        //premier facent d'ajouter un patient
+        Patient patient = new Patient();
+        patient.setId(null);
+        patient.setNom("Oussama");
+        patient.setDateNaissance(new Date());
+        patient.setMalade(false);
+        patient.setScore(23);
+
+        //Deuxieme facent d'ajouter un patient
+        Patient patient2 = new Patient(null,"Yassine",new Date(),false,123);
+
+        //Troisieme facent d'ajouter un patient en utilisant builder
+        Patient patient3 = Patient.builder()
+                .nom("Imane")
+                .dateNaissance(new Date())
+                .score(56)
+                .malade(true)
+                .build();
+    }
 }
---
+++ src/main/java/ma/enset/hopital/repository/PatientRepository.java
@@ -0,0 +1,7 @@
+package ma.enset.hopital.repository;
+
+import ma.enset.hopital.entities.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+}
