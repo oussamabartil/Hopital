3b708ecd2a4201f5d6fb5a4e899b32cb73870b1d
oussamabartil <oussamabartil.04@gmail.com>
2025-03-17 11:00:28 +0000

    Ajout de la vue Thymeleaf pour afficher la liste des patients

--- src/main/java/ma/enset/hopital/web/PatientController.java
+++ src/main/java/ma/enset/hopital/web/PatientController.java
@@ -1,9 +1,13 @@
 package ma.enset.hopital.web;
 
 import lombok.AllArgsConstructor;
+import ma.enset.hopital.entities.Patient;
 import ma.enset.hopital.repository.PatientRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
@@ -11,7 +15,9 @@ public class PatientController {
 
     private PatientRepository patientRepository;
     @GetMapping("/index")
-    public String index(){
+    public String index(Model model){
+        List<Patient> patientList = patientRepository.findAll();
+        model.addAttribute("ListsPatients", patientList);
         return "patients";
     }
 }
