aa02fd99f090eb048042c2bdbf11f6dd46cc7b5a
oussamabartil <oussamabartil.04@gmail.com>
2025-03-19 18:28:37 +0000

    Ajout de la recherche de patients par nom avec Spring Data JPA et HQL

--- src/main/java/ma/enset/hopital/repository/PatientRepository.java
+++ src/main/java/ma/enset/hopital/repository/PatientRepository.java
@@ -1,7 +1,18 @@
 package ma.enset.hopital.repository;
 
 import ma.enset.hopital.entities.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    //Premier solution
+    Page<Patient> findByNomContains(String keyword, Pageable pageable);
+
+    //deuxieme solution d'utiliser HQL
+    @Query("select p from Patient p where p.nom like :x")
+    Page<Patient> chercher(@Param("x") String keyword, Pageable pageable);
+
 }
