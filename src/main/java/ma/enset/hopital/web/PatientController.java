c8129e9401cb560a37a8dc8a9f55c3802f73de23
oussamabartil <oussamabartil.04@gmail.com>
2025-03-19 18:02:06 +0000

    Ajout de la pagination dynamique avec paramètres de requête pour la gestion des patients

--- src/main/java/ma/enset/hopital/web/PatientController.java
+++ src/main/java/ma/enset/hopital/web/PatientController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -17,10 +18,15 @@ public class PatientController {
 
     private PatientRepository patientRepository;
     @GetMapping("/index")
-    public String index(Model model,int page ,int size){
+    public String index(Model model,
+                        @RequestParam(name="page",defaultValue = "0")
+                        int page ,
+                        @RequestParam(name="size",defaultValue = "4")
+                        int size){
         //Pagination
         Page<Patient> pagePatients = patientRepository.findAll(PageRequest.of(page,size));
         model.addAttribute("ListsPatients", pagePatients.getContent());
+        model.addAttribute("pages",new int[pagePatients.getTotalPages()]);
         return "patients";
     }
 }
