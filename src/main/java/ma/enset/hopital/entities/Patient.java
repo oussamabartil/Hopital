976392abc2f243cc1c793c84165ed3442fc104c5
oussamabartil <oussamabartil.04@gmail.com>
2025-03-24 10:32:51 +0000

    Formater la date de naissance dans model Patient

--- src/main/java/ma/enset/hopital/entities/Patient.java
+++ src/main/java/ma/enset/hopital/entities/Patient.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 @Entity
@@ -16,6 +17,7 @@ public class Patient {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nom;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dateNaissance;
     private boolean malade;
     private int score;
