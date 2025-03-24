f63e4670704aa49817f79d68333fd7f741513337
oussamabartil <oussamabartil.04@gmail.com>
2025-03-24 11:24:21 +0000

    Mise à jour du formulaire pour conserver les paramètres `page` et `keyword` après la sauvegarde du patient.

--- src/main/java/ma/enset/hopital/web/PatientController.java
+++ src/main/java/ma/enset/hopital/web/PatientController.java
@@ -56,10 +56,11 @@ public class PatientController {
     }
 
     @PostMapping(path ="/save")
-    public String save(Model model, @Valid Patient patient, BindingResult bindingResult){
+    public String save(Model model, @Valid Patient patient, BindingResult bindingResult,@RequestParam(defaultValue = "0") int page,
+                                                                                         @RequestParam(defaultValue = "") String keyword){
         if(bindingResult.hasErrors()) return "formPatients";
         patientRepository.save(patient);
-        return "redirect:/index";
+        return "redirect:/index?page="+page+"&keyword="+keyword;
     }
 
     @GetMapping("/editPatient")
