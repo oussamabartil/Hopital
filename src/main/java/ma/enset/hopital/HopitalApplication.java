163b1c8ae6fd16de4317fe014b75d017f0c124da
oussamabartil <oussamabartil.04@gmail.com>
2025-04-19 16:02:28 +0000

    Ajout de l'encodage des mots de passe avec BCrypt dans la configuration de sécurité

--- src/main/java/ma/enset/hopital/HopitalApplication.java
+++ src/main/java/ma/enset/hopital/HopitalApplication.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Date;
 
@@ -42,5 +45,10 @@ public class HopitalApplication implements CommandLineRunner {
 //        patientRepository.save(new Patient(null,"Hanane",new Date(),false,3334));
 //        patientRepository.save(new Patient(null,"Mohammed",new Date(),true,114));
 
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/ma/enset/hopital/security/SecurityConfig.java
+++ src/main/java/ma/enset/hopital/security/SecurityConfig.java
@@ -1,22 +1,26 @@
 package ma.enset.hopital.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
         return new InMemoryUserDetailsManager(
-                User.withUsername("user1").password("{noop}1234").roles("USER").build(),
-                User.withUsername("user2").password("1234").roles("USER").build(),
-                User.withUsername("admin").password("1234").roles("USER","ADMIN").build()
+                User.withUsername("user1").password(passwordEncoder.encode("1234")).roles("USER").build(),
+                User.withUsername("user2").password(passwordEncoder.encode("1234")).roles("USER").build(),
+                User.withUsername("admin").password(passwordEncoder.encode("1234")).roles("USER","ADMIN").build()
         );
     }
     @Bean
