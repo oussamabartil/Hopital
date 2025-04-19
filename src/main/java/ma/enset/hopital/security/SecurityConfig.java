124369b52b40d2e26cb429051103f5b141d0670f
oussamabartil <oussamabartil.04@gmail.com>
2025-04-19 15:50:47 +0000

    Ajout d'une configuration de sécurité avec des utilisateurs en mémoire

--- src/main/java/ma/enset/hopital/security/SecurityConfig.java
+++ src/main/java/ma/enset/hopital/security/SecurityConfig.java
@@ -4,11 +4,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user1").password("{noop}1234").roles("USER").build(),
+                User.withUsername("user2").password("1234").roles("USER").build(),
+                User.withUsername("admin").password("1234").roles("USER","ADMIN").build()
+        );
+    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
       httpSecurity.formLogin();
